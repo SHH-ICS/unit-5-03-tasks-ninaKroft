ab6c942b852419346b88be196f2bf5cec92b281c
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-05-16 14:46:18 +0000

    somewhat complete

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,2 +1,37 @@
 # Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
 # This will be six lines, and then store the questions in the file questions.txt.
+
+numberOfQuestions = int(input("How many questions do you want in your quiz? You can have a maximum of 10 questions."+'\n'))
+
+prefix = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eight", "ninth", "tenth"]
+
+questions = open("questions.txt", 'w')
+
+for x in range(0, numberOfQuestions):
+    print("Please enter your", prefix[x], "question:")
+    question = input()
+    questions.write(question +'\n')
+
+    print("Enter the first answer:")
+    first = input()
+    questions.write(first+'\n')
+
+    print("Enter the second answer:")
+    second = input()
+    questions.write(second+'\n')
+
+    print("Enter the third answer:")
+    third = input()
+    questions.write(third+'\n')
+
+    print("Enter the fourth answer:")
+    fourth = input()
+    questions.write(fourth+'\n')
+
+    print("Which answer is correct? Enter A, B, C, or D.")
+    answer = input()
+    questions.write(answer)
+
+print("Your questions are set! Head over to Q2Quiz.py to run your quiz.")
+questions.close()
+
--- Q2Quiz.py
+++ Q2Quiz.py
@@ -1,2 +1,26 @@
 # Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
 # The program will keep score of the number of questions answered correctly.
+
+quiz = open("questions.txt", 'r')
+
+
+letters = ["A.)", "B.)", "C.)", "D.)"]
+
+question = quiz.readline()
+print(question)
+
+for x in range(0, 4):
+    answers = quiz.readline()
+    letter = letters[x]
+    print(letter, answers)
+
+answer = quiz.readline()
+
+userAnswer = input("What is the answer?"+'\n')
+
+#if answer == userAnswer:
+    #print("Correct")
+#else:
+    #print("Incorrect")
+
+quiz.close()
