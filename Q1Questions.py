674cd612cbde08b9653701d524538b2320f58d5f
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-05-18 13:46:29 +0000

    another loop

--- Q1Questions.py
+++ Q1Questions.py
@@ -12,21 +12,10 @@ for x in range(0, numberOfQuestions):
     question = input()
     questions.write(question +'\n')
 
-    print("Enter the first answer:")
-    first = input()
-    questions.write(first+'\n')
-
-    print("Enter the second answer:")
-    second = input()
-    questions.write(second+'\n')
-
-    print("Enter the third answer:")
-    third = input()
-    questions.write(third+'\n')
-
-    print("Enter the fourth answer:")
-    fourth = input()
-    questions.write(fourth+'\n')
+    for y in range(0, 4):
+        print("Enter the", prefix[y], "answer:")
+        option = input()
+        questions.write(option+'\n')
 
     print("Which answer is correct? Enter A, B, C, or D.")
     answer = input()
@@ -35,4 +24,3 @@ for x in range(0, numberOfQuestions):
 print("Your questions are set! Head over to Q2Quiz.py to run your quiz.")
 
 questions.close()
-
